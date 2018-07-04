11c5fba7dcb2443ff0eab1b93823613d173369df
George Daramouskas <gdaramouskas@therp.nl>
2018-07-04 13:26:52 +0300

    [PORT] Forward porting project_scrum to 10.0

---
+++ project_scrum/models/project_scrum_actors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright <2017> <Tenovar Ltd>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models, fields
+
+
+class ProjectScrumActors(models.Model):
+    _name = 'project.scrum.actors'
+    _description = 'Actors in user stories'
+
+    name = fields.Char(required=True)
