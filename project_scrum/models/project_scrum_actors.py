58258ccb68e96888b2cce61c2916963a76853001
Fernando La Chica <fernando.lachica@guadaltech.es>
2020-11-21 19:06:06 +0100

    [IMP] project_scrum: black, isort

--- project_scrum/models/project_scrum_actors.py
+++ project_scrum/models/project_scrum_actors.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright <2017> <Tenovar Ltd>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectScrumActors(models.Model):
-    _name = 'project.scrum.actors'
-    _description = 'Actors in user stories'
+    _name = "project.scrum.actors"
+    _description = "Actors in user stories"
 
     name = fields.Char(required=True)
--- project_scrum/models/project_scrum_meeting.py
+++ project_scrum/models/project_scrum_meeting.py
@@ -1,99 +1,79 @@
-# -*- coding: utf-8 -*-
 # Copyright <2017> <Tenovar Ltd>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 
 
 class ProjectScrumMeeting(models.Model):
-    _name = 'project.scrum.meeting'
-    _description = 'Project Scrum Daily Meetings'
-    _inherit = ['mail.thread', 'ir.needaction_mixin']
+    _name = "project.scrum.meeting"
+    _description = "Project Scrum Daily Meetings"
+    _inherit = ["mail.thread", "ir.needaction_mixin"]
 
     project_id = fields.Many2one(
-        comodel_name='project.project',
-        string='Project',
-        ondelete='cascade',
+        comodel_name="project.project",
+        string="Project",
+        ondelete="cascade",
         index=True,
-        track_visibility='onchange',
+        track_visibility="onchange",
         change_default=True,
         required=True,
     )
-    sprint_id = fields.Many2one(
-        comodel_name='project.scrum.sprint',
-        string='Sprint',
-    )
+    sprint_id = fields.Many2one(comodel_name="project.scrum.sprint", string="Sprint",)
     datetime_meeting = fields.Datetime(
-        string='Date and Time of the meeting',
-        required=True,
+        string="Date and Time of the meeting", required=True,
     )
     user_id_meeting = fields.Many2one(
-        comodel_name='res.users',
-        string='Meeting Organizer',
+        comodel_name="res.users",
+        string="Meeting Organizer",
         required=True,
-        default=lambda self: self.env.user
+        default=lambda self: self.env.user,
     )
-    question_yesterday = fields.Html(
-        string='Description',
-        required=True,
-    )
-    question_today = fields.Html(
-        string='Description',
-        required=True,
-    )
-    question_blocks = fields.Html(
-        string='Description',
-        required=False,
-    )
+    question_yesterday = fields.Html(string="Description", required=True,)
+    question_today = fields.Html(string="Description", required=True,)
+    question_blocks = fields.Html(string="Description", required=False,)
     question_backlog = fields.Selection(
-        [('yes', 'Yes'), ('no', 'No')],
-        string='Backlog Accurate?',
+        [("yes", "Yes"), ("no", "No")],
+        string="Backlog Accurate?",
         required=False,
-        default='yes',
+        default="yes",
     )
-    company_id = fields.Many2one(
-        related='project_id.analytic_account_id.company_id',
-    )
+    company_id = fields.Many2one(related="project_id.analytic_account_id.company_id",)
 
     @api.multi
     def name_get(self):
         result = []
         for rec in self:
-            name = ''
+            name = ""
             if rec.project_id:
-                name = "%s - %s - %s" % (
+                name = "{} - {} - {}".format(
                     rec.project_id.name,
                     rec.user_id_meeting.name,
-                    rec.datetime_meeting)
+                    rec.datetime_meeting,
+                )
             else:
-                name = "%s - %s" % (
-                    rec.user_id_meeting.name,
-                    rec.datetime_meeting)
+                name = "{} - {}".format(rec.user_id_meeting.name, rec.datetime_meeting)
             result.append((rec.id, name))
         return result
 
     @api.multi
     def send_email(self):
         self.ensure_one()
-        template = self.env.ref('project_scrum.email_template_id', False)
-        compose_form = self.env.ref(
-            'mail.email_compose_message_wizard_form',
-            False,
-        )
+        template = self.env.ref("project_scrum.email_template_id", False)
+        compose_form = self.env.ref("mail.email_compose_message_wizard_form", False,)
         ctx = dict(
-            default_model='project.scrum.meeting',
+            default_model="project.scrum.meeting",
             default_res_id=self.id,
             default_use_template=bool(template),
             default_template_id=template.id,
-            default_composition_mode='comment',
+            default_composition_mode="comment",
         )
         return {
-            'name': _('Compose Email'),
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'mail.compose.message',
-            'views': [(compose_form.id, 'form')],
-            'view_id': compose_form.id,
-            'target': 'new',
-            'context': ctx,
+            "name": _("Compose Email"),
+            "type": "ir.actions.act_window",
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "mail.compose.message",
+            "views": [(compose_form.id, "form")],
+            "view_id": compose_form.id,
+            "target": "new",
+            "context": ctx,
         }
